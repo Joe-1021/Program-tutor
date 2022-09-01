1bc46a0fd96e70b56a40d66f057b3446b13b8c15
yoyo <a4253607@gmail.com>
2022-09-02 00:59:50 +0800

    Update 2022-09-01T16:59:48.246Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,12 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".ProgramSwiper", {
+  slidesPerView: 7,
+  spaceBetween: 16
+});
+var swiper = new Swiper(".CourseSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 16
+});
 //# sourceMappingURL=all.js.map
