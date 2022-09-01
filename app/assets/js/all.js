e2a04dfddce7542e2e89512c87a457fb6c9dd064
yoyo <a4253607@gmail.com>
2022-09-02 01:01:24 +0800

    依助教修正

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,13 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+var swiper = new Swiper(".ProgramSwiper", {
+        slidesPerView: 7,
+        spaceBetween: 16,
+} );
+
+var swiper = new Swiper(".CourseSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+} );
